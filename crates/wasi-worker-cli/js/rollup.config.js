0312b0bb569d1c1775371c17e4d32d74369948fb
Maxim Vorobjov <maxim.vorobjov@gmail.com>
2020-08-28 16:14:45 +0300

    Upgrade wasmer-js to version 0.11

--- crates/wasi-worker-cli/js/rollup.config.js
+++ crates/wasi-worker-cli/js/rollup.config.js
@@ -1,40 +1,37 @@
 // Rollup Config for Web Workers
 
-import resolve from "rollup-plugin-node-resolve";
-//import commonjs from "rollup-plugin-commonjs";
-//import builtins from "rollup-plugin-node-builtins";
-//import globals from "rollup-plugin-node-globals";
-import json from "rollup-plugin-json";
-import typescript from "rollup-plugin-typescript2";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import typescript from "@rollup/plugin-typescript";
 import compiler from "@ampproject/rollup-plugin-closure-compiler";
-import bundleSize from "rollup-plugin-bundle-size";
-import copy from 'rollup-plugin-copy';
+import replace from '@rollup/plugin-replace';
+import globals from "rollup-plugin-node-globals";
+import builtins from "rollup-plugin-node-builtins";
+//import copy from 'rollup-plugin-copy';
+
+// Allowing to compile wasmer-js for browser
+const replaceBrowserOptions = {
+    delimiters: ["", ""],
+    "/*ROLLUP_REPLACE_BROWSER": "",
+    "ROLLUP_REPLACE_BROWSER*/": "",
+    'Object.defineProperty(exports, "__esModule", { value: true });': "",
+};
 
 const sourcemapOption = process.env.PROD ? undefined : "inline";
 
-let typescriptPluginOptions = {
-  tsconfig: "./tsconfig.json",
-  exclude: ["./test/**/*"],
-  clean: process.env.PROD ? true : false,
-  objectHashIgnoreUnknownHack: true
-};
-
 const plugins = [
+  replace(replaceBrowserOptions),
+  typescript(),
   resolve({
-    preferBuiltins: true
+    preferBuiltins: false,
   }),
-  copy({
-    targets: [
-      { src: 'node_modules/@wasmer/wasm-transformer/LICENSE', dest: 'dist' },
-      { src: 'node_modules/@wasmer/wasm-transformer/wasm_transformer_bg.wasm', dest: 'dist' },
-//      { src: `target/wasm32-wasi/${process.env.PROD ? "release" : "debug"}/worker.wasm`, dest: 'static/bin' },
-    ]
-  }),
-  // just for debugging of @wasmer/wasi
-  typescript(typescriptPluginOptions),
-  json(),
+  commonjs({
+      transformMixedEsModules: true,
+    }),
+  globals(),
+  builtins(),
   process.env.PROD ? compiler() : undefined,
-  process.env.PROD ? bundleSize() : undefined
 ];
 
 const workerBundles = [
@@ -42,7 +39,8 @@ const workerBundles = [
     input: "./src/worker.ts",
     output: [
       {
-        file: "dist/worker.js",
+        dir: "dist",
+//        file: "dist/worker.js",
         format: "iife",
         sourcemap: sourcemapOption,
         name: "Process"
